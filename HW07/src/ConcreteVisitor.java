8a08d5bb03e2bac53730d3f04cde70e29c2a562e
alnom <alnom@LAPTOP-SSKDJCRJ>
2022-11-27 13:42:52 +0100

    Update

--- HW07/src/ConcreteVisitor.java
+++ HW07/src/ConcreteVisitor.java
@@ -24,17 +24,17 @@ public class ConcreteVisitor implements Visitor {
 	public void cost(Electronic electronic) {
 		double cost = 0;
 		if (electronic.getFragile() == true) {
-			cost = 5*(electronic.getWeight()/10);
+			cost = 5*((double)electronic.getWeight()/10);
 			System.out.println("Total Cost for " + electronic.getName() + " is: " + cost);
 		}else {
-			cost = 5*(electronic.getWeight()/15);
+			cost = 5*((double)electronic.getWeight()/15);
 			System.out.println("Total Cost for " + electronic.getName() + " is: " + cost);
 		}
 	}
 
 	@Override
 	public void cost(Furniture furniture) {
-		double cost = 5*(furniture.getWeight()/20);
+		double cost = 5*((double)furniture.getWeight()/20);
 		System.out.println("Total Cost for " + furniture.getName() + " is: " + cost);	
 		
 	}
@@ -42,20 +42,20 @@ public class ConcreteVisitor implements Visitor {
 	@Override
 	public void packing(Glass glass) {
 		System.out.println(glass.getName()+ ":");
-		System.out.println("    >should be filled with Bubble wraps and packed in a box with a dimension : " + (glass.getLenght()+1) + "x" + (glass.getWidth()+1) + "x" + (glass.getHeight()+1));
+		System.out.println("    >should be filled with Bubble wraps and packed in a box with a dimension : " + (glass.getHeight()+1) + "x" + (glass.getLenght()+1) + "x" + (glass.getWidth()+1));
 		
 	}
 
 	@Override
 	public void packing(Electronic electronic) {
 		System.out.println(electronic.getName() + ":");
-		System.out.println("    >should be covered with Polyethylene foam film and packed in a box with a dimension : " + (electronic.getLenght()+1) + "x" + (electronic.getWidth()+1) + "x" + (electronic.getHeight()+1));	
+		System.out.println("    >should be covered with Polyethylene foam film and packed in a box with a dimension : " + (electronic.getHeight()+1) + "x" + (electronic.getLenght()+1) + "x" + (electronic.getWidth()+1));	
 	}
 
 	@Override
 	public void packing(Furniture furniture) {
 		System.out.println(furniture.getName() + ":");
-		System.out.println("    >should be covered with waterproof covers with area of : " + (furniture.getLenght())+ "x" + (furniture.getWidth()) + "x" + (furniture.getHeight()));
+		System.out.println("    >should be covered with waterproof covers with area of : " + (furniture.getLenght())+ "x" + (furniture.getWidth()));
 		
 	}
 
